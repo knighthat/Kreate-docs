f9df10c146a5e7cf7f56d03539153660cf072ce8
knighthat <git@knighthat.me>
2025-06-22 00:44:26 -0500

    merge HomepageHeader to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,18 @@ import styles from './index.module.css';
 import HomepageDoctype from '@site/src/components/homepage/doctype';
 import Banners from '@site/src/components/homepage/banner';
 
-function HomepageHeader() {
-  return (
-    <header className={styles.heroBanner}>
-      <div className="container">
-        <Banners />
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      <header className={styles.heroBanner}>
+        <div className="container">
+          <Banners />
+        </div>
+      </header>
+      
       <main>
         <HomepageDoctype />
       </main>

9c70ed6299ff692908388abbc9db104acc417f75
knighthat <git@knighthat.me>
2025-10-01 01:05:07 -0500

    replace 3-image banner with single wide banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import type {ReactNode} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-import styles from './index.module.css';
-
 import HomepageDoctype from '@site/src/components/homepage/doctype';
-import Banners from '@site/src/components/homepage/banner';
+
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
@@ -13,11 +11,7 @@ export default function Home(): ReactNode {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <header className={styles.heroBanner}>
-        <div className="container">
-          <Banners />
-        </div>
-      </header>
+      <img src='/img/banner.webp' alt='banner' />
       
       <main>
         <HomepageDoctype />

b840e98aa6cc73216766efb6bfd2f35a63d0653b
knighthat <git@knighthat.me>
2025-10-16 17:45:06 -0500

    add icon suport to navbar navlink

--- src/theme/NavbarItem/NavbarNavLink.tsx
+++ src/theme/NavbarItem/NavbarNavLink.tsx
@@ -1,10 +1,46 @@
-import React, {type ReactNode} from 'react';
+import React, {type ReactNode, useState, useEffect } from 'react';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 import {isRegexpStringMatch} from '@docusaurus/theme-common';
 import IconExternalLink from '@theme/Icon/ExternalLink';
 import type {Props} from '@theme/NavbarItem/NavbarNavLink';
+import { useColorMode } from '@docusaurus/theme-common';
+
+import { Icon } from '@iconify/react';
+
+
+interface IconProps {
+  name: string
+  color?: string
+  colorDark?: string
+}
+
+interface NavItenProps extends Props {
+  icon?: IconProps
+}
+
+function NavLinkIcon({ icon: { name, color, colorDark } }: { icon: IconProps }): ReactNode {
+  const { colorMode } = useColorMode();
+  const [iconColor, setIconColor] = useState('var(--ifm-color-emphasis-1000)')
+  useEffect(() => {
+      const newColor = colorMode == 'dark' && colorDark
+        ? colorDark
+        : color || 'var(--ifm-color-emphasis-1000)';
+
+      setIconColor(newColor);
+  }, [colorMode]);
+
+  return (
+    <div className="row">
+      <Icon 
+        icon={name} 
+        className='col' 
+        width='2rem' 
+        color={iconColor} />
+    </div>
+  )
+}
 
 export default function NavbarNavLink({
   activeBasePath,
@@ -15,8 +51,9 @@ export default function NavbarNavLink({
   html,
   isDropdownLink,
   prependBaseUrlToHref,
+  icon,
   ...props
-}: Props): ReactNode {
+}: NavItenProps): ReactNode {
   // TODO all this seems hacky
   // {to: 'version'} should probably be forbidden, in favor of {to: '/version'}
   const toUrl = useBaseUrl(to);
@@ -24,8 +61,15 @@ export default function NavbarNavLink({
   const normalizedHref = useBaseUrl(href, {forcePrependBaseUrl: true});
   const isExternalLink = label && href && !isInternalUrl(href);
 
-  // Link content is set through html XOR label
-  const linkContentProps = html
+
+  // Link content is set through html, icon, XOR label
+  const linkContentProps = icon
+    ? {
+      children: (
+        <NavLinkIcon icon={icon} />
+      )
+    }
+    : html
     ? {dangerouslySetInnerHTML: {__html: html}}
     : {
         children: (
